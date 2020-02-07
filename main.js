cd786fe44737d292f92a877e04a1a78b6f2a5ee4
Kurenyshev Dmitry <itdvtech@gmail.com>
2020-02-07 15:30:17 +0500

    дз к 1 уроку.

---
+++ main.js
@@ -0,0 +1,15 @@
+"use strict";
+
+/* 1 задание */
+
+let tc = +prompt("Введите температуру по цельсию:");
+let tf = (9 / 5) * tc + 32;
+alert(tf);
+
+
+/* 2 задание */
+
+let admin;
+let name = "Василий";
+admin = name;
+alert(admin);
